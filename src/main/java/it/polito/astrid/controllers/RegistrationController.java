d11a83e60f393aac67e434e9aac48d48834391c8
yujboss <yujboss@gmail.com>
2019-06-28 20:50:00 +0200

    updated swagger, fixed log outputs

--- src/main/java/it/polito/astrid/controllers/RegistrationController.java
+++ src/main/java/it/polito/astrid/controllers/RegistrationController.java
@@ -6,6 +6,7 @@ import java.util.Arrays;
 import java.util.Collections;
 
 import javax.servlet.http.HttpServletResponse;
+import javax.validation.Valid;
 import javax.xml.bind.JAXBContext;
 import javax.xml.bind.JAXBException;
 import javax.xml.bind.Marshaller;
@@ -22,6 +23,10 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.client.RestTemplate;
 
+import io.swagger.annotations.ApiOperation;
+import io.swagger.annotations.ApiParam;
+import io.swagger.annotations.ApiResponse;
+import io.swagger.annotations.ApiResponses;
 import it.polito.verefoo.astrid.jaxb.InfrastructureInfo;
 import it.polito.verefoo.jaxb.NFV;
 
@@ -35,11 +40,17 @@ public class RegistrationController {
 
 	
 	
-	@RequestMapping(method = RequestMethod.POST, value = "/register/insfrastructure")
+	
+	@ApiOperation(value = "registerInfrastructure", notes = "Recieves Infrastructure info and sends it to Verikube. Waits for result and sends it back", response = NFV.class)
+	@RequestMapping(method = RequestMethod.POST, value = "/register/insfrastructure", produces = "application/xml", consumes="application/xml")
+	@ApiResponses(value = {
+	    		@ApiResponse(code = 201, message = "Created"),
+	    		@ApiResponse(code = 400, message = "Bad Request"),
+	    		})
 	@ResponseBody
-	public NFV registerInfrastructure(@RequestBody InfrastructureInfo info) {
+	public NFV registerInfrastructure( @ApiParam(value = "Infrastructure Info", required = true) @RequestBody InfrastructureInfo info) {
 		if(info==null||info.getMetadata()==null||info.getMetadata().getName()==null) {
-			System.out.println("+++++++ Empty body");
+			System.out.println("++++++++++ registerInfrastructure Empty body");
 			return null;
 		}
 		
@@ -54,27 +65,26 @@ public class RegistrationController {
 		 * 
 		 */
 		
-		System.out.println("---"+info);
 	
 		HttpHeaders headers = new HttpHeaders();
 		headers.setContentType(MediaType.APPLICATION_XML);
 		
-		System.out.println("+++++++ Obtained IN info: "+info.getMetadata().getName());
+		System.out.println("++++++++++ registerInfrastructure Controller Obtained Infrastructure info for: "+info.getMetadata().getName());
 		RestTemplate restTemplate = new RestTemplate();
 		Jaxb2RootElementHttpMessageConverter converter = new Jaxb2RootElementHttpMessageConverter();
 		converter.setSupportedMediaTypes(Collections.singletonList(MediaType.ALL));
 		restTemplate.setMessageConverters(Arrays.asList(converter,new StringHttpMessageConverter()));
 		// Data attached to the request.
 		HttpEntity<InfrastructureInfo> requestBody = new HttpEntity<InfrastructureInfo>(info,headers);
-		System.out.println("--- "+headers.toString());
 		
 		
 		
-		System.out.println("+++++++ Sending to Verekube");
+		System.out.println("++++++++++ registerInfrastructure Controller Sending Infrastructure info to Verekube");
 		// Send request with POST method.
 		NFV result = restTemplate.postForObject(URL_DC, requestBody, NFV.class);
+		
 		/*
-		 * System.out.println("Status code:" + result.getStatusCode());
+		 * 
 		 * 
 		 * 
 		 * // Code = 200. if (result.getStatusCode() == HttpStatus.OK) {
@@ -84,27 +94,30 @@ public class RegistrationController {
 		return result;
 	}
 	
-	@RequestMapping(method = RequestMethod.POST, value = "/register/policy")
+	@ApiOperation(value = "registerPolicy", notes = "Recieves Policies as String and sends it to Verikube. ")
+	@RequestMapping(method = RequestMethod.POST, value = "/register/policy", produces = "text/plain", consumes="text/plain")
+	@ApiResponses(value = {
+	    		@ApiResponse(code = 201, message = "Created"),
+	    		@ApiResponse(code = 400, message = "Bad Request"),
+	    		})
 	@ResponseBody
 	public String registerPolicy(@RequestBody String policy) {
 		HttpHeaders headers = new HttpHeaders();
 		headers.setContentType(MediaType.TEXT_PLAIN);
 		
-		System.out.println("------- Obtained Policy");
+		System.out.println("++++++++++ registerPolicy Controller Obtained Policy");
 		RestTemplate restTemplate = new RestTemplate();
 		
 		// Data attached to the request.
 		HttpEntity<String> requestBody = new HttpEntity<>(policy,headers);
 
-		System.out.println("------- Sending to Verekube");
+		System.out.println("++++++++++ registerPolicy Controller Sending to Verekube");
 		// Send request with POST method.
 		ResponseEntity<String> result = restTemplate.postForEntity(URL_POLICY, requestBody, String.class);
-		System.out.println("Status code:" + result.getStatusCode());
 		// Code = 200.
 		if (result.getStatusCode() == HttpStatus.OK) {
-			System.out.println("------- Success from Verekube: "+result.getBody());
+			System.out.println("++++++++++ registerPolicy Controller Success from Verekube: "+result.getBody());
 		}
-		System.out.println("------- Printing");
 		return result.getBody();
 	}
 
